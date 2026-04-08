ce3da013a5d04c371a28e921ea8049e581111674
frank <frank@schlottmann-goedde.de>
2026-04-08 16:47:03 +0200

    set user and password in init script

--- app/init.sh
+++ app/init.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-
+set -e
+DIST_FILE="init.sql.dist"
 INIT_FILE="init.sql"
-
 # env
 #exec tail -f /dev/null
-set -e
 
+cp $DEST_FILE $INIT_FILE
 
 if [ -n "$APP_USER" ]; then
 sed -i -e "s/APP_USER/$APP_USER/g" $INIT_FILE
@@ -14,6 +14,7 @@ fi
 if [ -n "$APP_PASSWORD" ]; then
    sed -i -e "s/APP_PASSWORD/$APP_PASSWORD/g" $INIT_FILE
 fi
+
 
 echo /opt/firebird/bin/isql -user SYSDBA -pass ${FIREBIRD_ROOT_PASSWORD:-masterkey} -i /home/shiva/bin/init.sql firebird:/var/lib/firebird/data/${FIREBIRD_DATABASE}
 /opt/firebird/bin/isql -user SYSDBA -pass ${FIREBIRD_ROOT_PASSWORD:-masterkey} -i /home/shiva/bin/init.sql firebird:/var/lib/firebird/data/${FIREBIRD_DATABASE}
